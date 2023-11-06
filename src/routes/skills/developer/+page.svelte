ace7c8e0c9a084426c17edbba3acd8b18a8962e5
Zachiah Sawyer <zachiah@proton.me>
2023-11-05 18:49:35 -0800

    add a bit to the developer section

--- src/routes/skills/developer/+page.svelte
+++ src/routes/skills/developer/+page.svelte
@@ -1,3 +1,19 @@
-<h1 class="text-4xl text-center">
-	TODO: Write something here. Pls check back later. Thanks... I'm a fullstack developer...
-</h1>
+<h1 class="text-4xl text-center mb-2">I am a fullstack developer</h1>
+
+<hr class="border-b border-black dark:border-white mb-4"/>
+
+<p class="mb-8">
+	here is my resume: <a
+		href="https://www.icloud.com/iclouddrive/0443CE8IOtGZWdu8d6yBpkiUw#resume"
+		class="underline hover:text-blue-800 dark:text-blue-200"
+	>
+		resume.pdf
+	</a>
+</p>
+<h2 class="text-2xl">Projects</h2>
+<ul class="list-disc list-inside">
+	<li><a class="underline hover:text-blue-800 dark:hover:text-blue-200" href="https://timeline.zachiah.com">Timeline of my life</a></li>
+    <li><a class="underline hover:text-blue-800 dark:hover:text-blue-200" href="https://svelte-conways-game-of-life.netlify.app/">Conway's Game of Life</a></li>
+    <li><a class="underline hover:text-blue-800 dark:hover:text-blue-200" href="https://github.com/Zachiah/lots-of-langs">lots-of-langs</a></li>
+    <li>TODO: Add more here</li>
+</ul>
