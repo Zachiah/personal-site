323008f3314b36f7ae8fe86dfedf66f7d396fd26
Zachiah Sawyer <zachiah@proton.me>
2023-11-07 21:02:43 -0800

    Make homepage simpler; Improve homepage links

--- src/routes/skills/+layout.svelte
+++ src/routes/skills/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
-<main class="mx-auto max-w-prose p-8 rounded-lg my-8 shadow-lg bg-blue-200 shadow-blue-400 dark:bg-blue-900 dark:shadow-blue-500 box-content">
-    <slot />
-</main>
+<div class="max-w-max mx-auto p-8">
+	<main
+		class="max-w-prose p-8 rounded-lg my-8 shadow-lg bg-blue-200 shadow-blue-400 dark:bg-blue-900 dark:shadow-blue-500 box-content"
+	>
+		<slot />
+	</main>
+</div>
