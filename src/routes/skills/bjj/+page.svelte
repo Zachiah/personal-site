2eca7096789160a91fea4617ddde98b1fefbe8ca
Zachiah Sawyer <zachiah@proton.me>
2024-02-12 08:13:40 -0800

    add note about cda jiujitsu to bjj section

--- src/routes/skills/bjj/+page.svelte
+++ src/routes/skills/bjj/+page.svelte
@@ -4,6 +4,8 @@
 
 <SEO title="Zachiah Sawyer | BJJ" description="Hi my name is Zachiah Sawyer. This is my BJJ journey"/>
 
-<p>I have been training at <a href="https://emergebjj.com/">Emerge BJJ</a> since February 2022</p>
+<p>I started training at <a href="https://emergebjj.com/">Emerge BJJ</a> since February 2022</p>
 
-<p>TODO: Write more here</p>
+<p>I got my blue belt on January 23 2024</p>
+
+<p>I started training at CDA Jiujitsu in February 2024</p>
