5951a52560ff88a2dfe9bdbfd91f2cc86b89fae7
Zachiah Sawyer <zachiah@proton.me>
2024-08-09 09:33:08 -0700

    increase margin on simple page wrapper

--- src/lib/components/SimplePageWrapper.svelte
+++ src/lib/components/SimplePageWrapper.svelte
@@ -5,7 +5,7 @@
 
 <div class="mx-auto max-w-max p-8">
 	<main
-		class="my-8 box-content max-w-prose rounded-lg bg-blue-100 p-8 shadow-lg shadow-sky-400 dark:bg-black dark:shadow-sky-500"
+		class="my-12 box-content max-w-prose rounded-lg bg-blue-100 p-8 shadow-lg shadow-sky-400 dark:bg-black dark:shadow-sky-500"
 	>
 		<Content>
             <slot />
