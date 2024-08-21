82360eabb4cfa7e34b7aac38c386122d21b0ec91
Zachiah Sawyer <zachiah@proton.me>
2024-08-21 11:38:23 -0700

    update to 81 backflips

--- src/routes/skills/trampoliner/+page.svelte
+++ src/routes/skills/trampoliner/+page.svelte
@@ -15,12 +15,12 @@
 		I am pretty good at single backflips, frontflips and front-halfs that's about it. I can do
 		front-fulls, back-fulls, back-halfs, sideflips, and such but they aren't perfect. What I excel
 		at is long strings of flips in a row (long as in scores). Here's my current record for
-		consecutive backflips (79).
+		consecutive backflips (81).
 	</p>
 
 	<div style="padding-bottom: 56.25%; position: relative;">
 		<iframe
-			title="Me doing 79 backflips"
+			title="Me doing 81 backflips"
 			style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"
 			width="100%"
 			height="100%"
@@ -28,5 +28,16 @@
 			frameborder="0"
 			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
 		/>
+
+		<iframe
+			title="Me doing 81 backflips"
+			style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"
+			width="100%"
+			height="100%"
+			src="https://www.youtube.com/embed/vUjGxRTS7PI?si=_YR4HcDxYxpv2XPE"
+			frameborder="0"
+			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+		/>
+
 	</div>
 </SimplePageWrapper>
