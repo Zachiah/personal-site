f038fda27e9c73aaa036cb93b90d670987d53011
Zachiah Sawyer <zachiah@proton.me>
2024-08-26 10:23:11 -0700

    improve styling on developer page

---
+++ src/routes/skills/developer/Project.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a class="text-center flex items-center justify-center flex-1 rounded-md bg-white p-4 dark:bg-black duration-200 hover:scale-110" href={href} target="_blank" rel="noopener">
+    <slot />
+</a>
